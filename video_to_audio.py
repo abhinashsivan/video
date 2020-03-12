7c4f696a55298e5a9eb20c232c3fb0c57fa3a321
abhinash_sivan <37009661+abhinashsivan@users.noreply.github.com>
2020-03-12 08:43:39 +0530

    Create video_to_audio.py

---
+++ video_to_audio.py
@@ -0,0 +1,14 @@
+import moviepy.editor
+
+#location of the video file
+vid = 'video.mp4'
+
+video = moviepy.editor.VideoFileClip(vid)
+
+audio = video.audio
+
+#saving the audio file with same file name as video
+audio_file=vid.replace(".mp4",".wav")
+audio.write_audiofile(audio_file)
+
+print("convertion completed  done\n\n")
